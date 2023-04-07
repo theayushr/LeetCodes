3817f1f9611abd32eb8c8b97edefa27ec5d930f9
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-04-08 00:39:45 +0530

    Binary Search in Sorted rotated array (logn)

---
+++ SearchSortedRotated/SearchSortedRotated.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+         //using binary search as logn
+
+         int start = 0, end = nums.size()-1;
+
+         while(start<=end){
+             int mid = start + (end-start)/2;
+
+             if(nums[mid] == target) return mid;
+
+                //is left half is sorted?
+             if(nums[start]<= nums[mid]){
+                 if(target >= nums[start] and target<=nums[mid]){
+                     end = mid -1;
+                 }
+                 else{
+                     start = mid+1;
+                 }
+             }
+             //right half is sorted
+             else{
+
+                 if(target >=nums[mid] and target <=nums[end]){
+                     start = mid+1;
+                 }
+                 else{
+                     end = mid-1;
+                 }
+
+             }
+
+         }
+
+         return -1;
+        
+    }
+};
