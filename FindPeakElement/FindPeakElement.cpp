dfd3a72b2371782c0e59af295a5098ce4ab22a97
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-04-15 22:51:35 +0530

    o(n)

---
+++ FindPeakElement/FindPeakElement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        /*
+        o(n)
+        */
+
+        for(int i=1; i<nums.size()-1; i++){
+            if(nums[i] > nums[i-1] and nums[i]> nums[i+1]){
+                return i;
+            }
+        }
+
+        return (nums[0]> nums[nums.size()-1])?0:nums.size()-1;
+        
+    }
+};
