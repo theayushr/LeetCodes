8737ef87ef7a01570fad6cd3ebd7a99ad2156ac0
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-04-19 14:43:52 +0530

    O(n^2)

---
+++ SetMatrixZeroes/SetMatrixZeros_n2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        vector<int> rows(row, 0);
+        vector<int> cols(col, 0);
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+
+                if(matrix[i][j]==0){
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        for(int i =0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(rows[i]==1 or cols[j]==1){
+                    matrix[i][j]= 0;
+                }
+            }
+        }
+
+    }
+};
