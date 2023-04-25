f8e05a12a456dbafb93add70736f342e179542e9
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-04-25 22:43:56 +0530

    o(n) linear search

---
+++ SearchIn2DMatrix/SearchIn2DMatrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int row = 0, col = cols-1;
+
+        while(row<rows and col >-1){
+            int curr = matrix[row][col];
+
+            if(curr==target) return true;
+
+            if(target > curr) row++;
+            else if(target < curr) col--;
+
+        }
+
+        return false;
+        
+    }
+};
