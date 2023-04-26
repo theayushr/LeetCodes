d4f6579628314ae2365135d11cfd68199c98f851
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-04-26 18:58:21 +0530

    Binary Search

---
+++ SingleElementInSortedArray/SingleElementInSortedArray3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+
+        int start = 0;
+        int end = nums.size()-2;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == nums[mid^1]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return nums[start];
+        
+    }
+};
