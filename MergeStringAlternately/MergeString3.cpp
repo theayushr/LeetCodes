897510eee389aef99ef7debe864e63b2b06ef072
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-06-17 11:33:03 +0530

    Single Pointer Approach

---
+++ MergeStringAlternately/MergeString3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+        string ans = "";
+
+        int n = max(word1.size(), word2.size());
+
+        for(int i=0; i<n; i++){
+            
+            if(i<word1.size()){
+                ans.push_back(word1[i]);
+            }
+
+            if(i<word2.size()){
+                ans.push_back(word2[i]);
+            }
+        }
+
+        
+        return ans;
+    }
+};
