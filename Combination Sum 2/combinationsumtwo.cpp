df5a9e6d528ef3b2c3aea39f9565f3ea0a28511b
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2023-06-24 08:30:31 +0530

    Recursion Striver

---
+++ Combination Sum 2/combinationsumtwo.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+
+    void findCombination(int ind, int target, vector<int>&arr, vector<vector<int>>&ans, vector<int>ds){
+
+        //base case
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i=ind; i<arr.size(); i++){
+
+            if(i>ind and arr[i] == arr[i-1]){
+                //not taking the duplicate elements
+                continue;
+            }
+
+            if(arr[i]>target){
+                break;
+            }
+
+            ds.push_back(arr[i]);
+            findCombination(i+1, target-arr[i], arr, ans, ds);
+            ds.pop_back();
+        }
+
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+        sort(candidates.begin(), candidates.end());
+        
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        findCombination(0, target, candidates, ans, ds);
+
+        return ans;
+    }
+};
